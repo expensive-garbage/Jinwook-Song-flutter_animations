b81e7e3dd7282703679435a3a8715000e41919dd
jinwook song <wlsdnr129@naver.com>
2023-05-17 20:10:43 +0900

    init

---
+++ lib/screens/implicit_animations_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ImplicitAnimationsScreen extends StatefulWidget {
+  const ImplicitAnimationsScreen({super.key});
+
+  @override
+  State<ImplicitAnimationsScreen> createState() =>
+      _ImplicitAnimationsScreenState();
+}
+
+class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
