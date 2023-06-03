f88419b69253d368723c7f3f2408e0e85892bc81
jinwook song <wlsdnr129@naver.com>
2023-06-03 22:29:36 +0900

    card with image

--- lib/screens/swiping_cards_screen.dart
+++ lib/screens/swiping_cards_screen.dart
@@ -12,6 +12,7 @@ class SwipingCardsScreen extends StatefulWidget {
 class _SwipingCardsScreenState extends State<SwipingCardsScreen>
     with SingleTickerProviderStateMixin {
   late final size = MediaQuery.of(context).size;
+  int _cardIndex = 1;
 
   late final AnimationController _animationController = AnimationController(
     vsync: this,
@@ -26,16 +27,32 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
     end: 15,
   );
 
+  late final Tween<double> _scale = Tween(
+    begin: 0.8,
+    end: 1.0,
+  );
+
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
     _animationController.value += details.delta.dx;
   }
 
+  void _dismissCard({required bool forward}) {
+    final factor = forward ? 1 : -1;
+    _animationController
+        .animateTo((size.width + 100) * factor)
+        .whenComplete(() {
+      _animationController.value = 0;
+      _cardIndex += 1;
+    });
+  }
+
   void _onHorizontalDragEnd(DragEndDetails details) {
-    final bound = size.width - 100;
-    if (_animationController.value >= bound) {
-      _animationController.animateTo(size.width + 100);
-    } else if (_animationController.value <= -bound) {
-      _animationController.animateTo(-(size.width + 100));
+    final bound = size.width / 2;
+    if (_animationController.value >= bound ||
+        _animationController.value <= -bound) {
+      _animationController.value.isNegative
+          ? _dismissCard(forward: false)
+          : _dismissCard(forward: true);
     } else {
       _animationController.animateTo(
         0,
@@ -65,30 +82,63 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
                   (_animationController.value + size.width / 2) / size.width) *
               pi /
               180; // convert to radians
-          return Stack(children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: GestureDetector(
-                onHorizontalDragUpdate: _onHorizontalDragUpdate,
-                onHorizontalDragEnd: _onHorizontalDragEnd,
-                child: Transform.translate(
-                  offset: Offset(_animationController.value, 100),
-                  child: Transform.rotate(
-                    angle: angle,
-                    child: Material(
-                      elevation: 20,
-                      color: Colors.red.shade100,
-                      child: SizedBox(
-                        width: size.width * 0.8,
-                        height: size.height * 0.5,
+
+          final scale = _scale
+              .transform(_animationController.value.abs() / (size.width + 100));
+
+          return Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Positioned(
+                top: 100,
+                child: Transform.scale(
+                    scale: scale,
+                    child: Card(
+                      index: (_cardIndex + 1) % 10,
+                    )),
+              ),
+              Positioned(
+                top: 100,
+                child: GestureDetector(
+                  onHorizontalDragUpdate: _onHorizontalDragUpdate,
+                  onHorizontalDragEnd: _onHorizontalDragEnd,
+                  child: Transform.translate(
+                    offset: Offset(_animationController.value, 0),
+                    child: Transform.rotate(
+                      angle: angle,
+                      child: Card(
+                        index: _cardIndex % 10,
                       ),
                     ),
                   ),
                 ),
               ),
-            )
-          ]);
+            ],
+          );
         },
+      ),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  final int index;
+  const Card({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Material(
+      elevation: 20,
+      borderRadius: BorderRadius.circular(10),
+      clipBehavior: Clip.hardEdge,
+      child: SizedBox(
+        width: size.width * 0.8,
+        height: size.height * 0.5,
+        child: Image.asset(
+          'assets/images/covers/yeonjae0$index.jpeg',
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
