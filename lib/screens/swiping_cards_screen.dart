5ebed4614ccbcd5a69f27530bc73a031d417e67b
jinwook song <wlsdnr129@naver.com>
2023-06-03 12:38:58 +0900

    swiping with animation

--- lib/screens/swiping_cards_screen.dart
+++ lib/screens/swiping_cards_screen.dart
@@ -7,19 +7,32 @@ class SwipingCardsScreen extends StatefulWidget {
   State<SwipingCardsScreen> createState() => _SwipingCardsScreenState();
 }
 
-class _SwipingCardsScreenState extends State<SwipingCardsScreen> {
-  double _posX = 0;
+class _SwipingCardsScreenState extends State<SwipingCardsScreen>
+    with SingleTickerProviderStateMixin {
+  late final size = MediaQuery.of(context).size;
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 2000),
+    lowerBound: -size.width,
+    upperBound: size.width,
+    value: 0,
+  );
 
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
-    setState(() {
-      _posX += details.delta.dx;
-    });
+    _animationController.value += details.delta.dx;
   }
 
   void _onHorizontalDragEnd(DragEndDetails details) {
-    setState(() {
-      _posX = 0;
-    });
+    _animationController.animateTo(
+      0,
+      curve: Curves.elasticOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -31,26 +44,31 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen> {
           'Swiping Cards',
         ),
       ),
-      body: Stack(children: [
-        Align(
-          alignment: Alignment.topCenter,
-          child: GestureDetector(
-            onHorizontalDragUpdate: _onHorizontalDragUpdate,
-            onHorizontalDragEnd: _onHorizontalDragEnd,
-            child: Transform.translate(
-              offset: Offset(_posX, 0),
-              child: Material(
-                elevation: 20,
-                color: Colors.red.shade100,
-                child: SizedBox(
-                  width: size.width * 0.8,
-                  height: size.height * 0.5,
+      body: AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, child) {
+          return Stack(children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: GestureDetector(
+                onHorizontalDragUpdate: _onHorizontalDragUpdate,
+                onHorizontalDragEnd: _onHorizontalDragEnd,
+                child: Transform.translate(
+                  offset: Offset(_animationController.value, 0),
+                  child: Material(
+                    elevation: 20,
+                    color: Colors.red.shade100,
+                    child: SizedBox(
+                      width: size.width * 0.8,
+                      height: size.height * 0.5,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-        )
-      ]),
+            )
+          ]);
+        },
+      ),
     );
   }
 }
